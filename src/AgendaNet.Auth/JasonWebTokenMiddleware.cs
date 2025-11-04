78c020f28d045e0ba9ecc224236365b7230df3cf
Danielle Mesquita <danielle.cmesquita@gmail.com>
2025-11-04 12:48:03 -0300

    fix(Auth):Correçoes do code Rabitt

--- src/AgendaNet.Auth/JasonWebTokenMiddleware.cs
+++ src/AgendaNet.Auth/JasonWebTokenMiddleware.cs
@@ -8,7 +8,6 @@ namespace AgendaNet.Auth
 {
     public static class JasonWebTokenMiddleware
     {
-        //private static string Auth = "Amordaminhavidadaquiateaeternidade";
         public static void AddJwtMiddleware(this IServiceCollection services, IConfiguration configuration)
         {
             var authKey = configuration["AUTHENTICATION"];
@@ -16,39 +15,36 @@ namespace AgendaNet.Auth
             {
                 throw new InvalidOperationException("AUTHENTICATION não configurada.");
             }
+
+            var key = Encoding.ASCII.GetBytes(authKey);
+
+
+            services.AddAuthorization(options =>
             {
-                var key = Encoding.ASCII.GetBytes(authKey);
-
-
-                services.AddAuthorization(options =>
+                options.AddPolicy("AdminPolicy", policy =>
                 {
-                    options.AddPolicy("AdminPolicy", policy =>
-                    {
-                        policy.RequireAuthenticatedUser();
-                        policy.RequireRole("Admin");
-                    });
+                    policy.RequireAuthenticatedUser();
+                    policy.RequireRole("Admin");
                 });
+            });
 
-                services.AddAuthentication(x =>
-                {
-                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                })
-               .AddJwtBearer(x =>
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+           .AddJwtBearer(x =>
+           {
+               x.RequireHttpsMetadata = true;
+               x.SaveToken = true;
+               x.TokenValidationParameters = new TokenValidationParameters
                {
-                   x.RequireHttpsMetadata = true;
-                   x.SaveToken = true;
-                   x.TokenValidationParameters = new TokenValidationParameters
-                   {
-                       ValidateIssuerSigningKey = true,
-                       IssuerSigningKey = new SymmetricSecurityKey(key),
-                       ValidateIssuer = false,
-                       ValidateAudience = false
-                   };
-               });
-
-
-            }
+                   ValidateIssuerSigningKey = true,
+                   IssuerSigningKey = new SymmetricSecurityKey(key),
+                   ValidateIssuer = false,
+                   ValidateAudience = false
+               };
+           });
         }
 
     }
