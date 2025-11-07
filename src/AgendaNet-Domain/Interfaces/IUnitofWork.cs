1161bcf107f33ed0b0a5f4a814dba502951721cc
Danielle Mesquita <danielle.cmesquita@gmail.com>
2025-11-07 18:53:18 -0300

    Develop:Repository(infra) (#12)
    * Update teste
    
    * feature(repository):Criando repostorios (#11)

---
+++ src/AgendaNet-Domain/Interfaces/IUnitofWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaNet_Domain.Interfaces
+{
+    public interface IUnitofWork: IDisposable
+    {
+        void BeginTransaction();
+        void CommitTransaction();
+        Task<int> CommitAsync();
+        void Rollback();
+    }
+}
---
+++ src/AgendaNet-Infra/Repositories/UserRepository.cs
@@ -0,0 +1,57 @@
+﻿using AgendaNet_Domain.Entities;
+using AgendaNet_Domain.Interfaces;
+using AgendaNet_Infra.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgendaNet_Infra.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly PostgreContext _context;
+
+        public UserRepository(PostgreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SaveAsync(User data)
+        {
+            _context.Entry(data).State = EntityState.Added;
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateAsync(User data)
+        {
+            _context.Users.Update(data);
+            _context.Entry(data).Property(p => p.IsActive).IsModified = false;
+            _context.Entry(data).Property(p => p.PasswordHash).IsModified = false;
+            await _context.SaveChangesAsync();
+        }
+        public Task<bool> DeleteAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<(List<User> items, int totalitems)> GetAllAsync(int pageindex, int pagesize)
+        {
+            throw new NotImplementedException();
+        }
+        #region Conctatt User
+        public async Task SaveContactAsync(Contact data)
+        {
+            _context.Entry(data).State = EntityState.Added;
+            await _context.SaveChangesAsync();
+        }
+        public Task UpdateContactAsync(Contact data)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<(List<Contact> items, int totalitems)> GetAllContactAsync(int pageindex, int pagesize)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<bool> DeleteConctactAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
